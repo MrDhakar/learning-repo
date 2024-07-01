81b01f919255e1a6284d760faa5e0e5ec740abb4
jaid <jaip.dhakar@gmail.com>
2024-07-01 12:29:11 +0530

    global exception handling

---
+++ api-gateway/src/main/java/com/jaid/gateway/Exceptions/UserNotFoundException.java
@@ -0,0 +1,24 @@
+package com.jaid.gateway.Exceptions;
+
+public class UserNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+	public UserNotFoundException() {
+		super();
+	}
+
+	public UserNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public UserNotFoundException(Throwable cause) {
+		super(cause);
+	}
+}
