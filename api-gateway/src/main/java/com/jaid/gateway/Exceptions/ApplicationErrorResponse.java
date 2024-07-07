780f6a3cd290dce1020edbc30003a864acf1f703
MrDhakar <jaip.dhakar@gmail.com>
2024-07-07 13:12:47 +0530

    changes in gateway server

---
+++ api-gateway/src/main/java/com/jaid/gateway/Exceptions/ApplicationErrorResponse.java
@@ -0,0 +1,19 @@
+package com.jaid.gateway.Exceptions;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class ApplicationErrorResponse {
+	 	private final String guid;
+	    private final String errorCode;
+	    private final String message;
+	    private final Integer statusCode;
+	    private final String statusName;
+	    private final String path;
+	    private final String method;
+	    private final LocalDateTime timestamp;
+		
+	    
+}
